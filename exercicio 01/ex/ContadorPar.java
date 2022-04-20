b24b56f5e715ef3e6c6763ebb39d07064e9c56ca
Pedro Ivo <85446995+IvoPenha@users.noreply.github.com>
2022-04-20 07:24:53 -0300

    Update ContadorPar.java

--- exercicio 01/ex/ContadorPar.java
+++ exercicio 01/ex/ContadorPar.java
@@ -4,28 +4,25 @@ public class ContadorPar {
 
 	public static void main(String[] args) {
 	
-			System.out.println(numerosPares(32,1));
+			numerosPares(32,1);
 
 	}
 
-    public static int numerosPares(int numero,int count) {	
-    		if (numero<=0){
-    			count++;
+    public static void numerosPares(int numero,int count) {	
+    		if (numero<=2){
+    			System.out.println(count);
     		}else {
     			if(isPar(numero)&&(numero!=0)){
         		count++;
         	    numerosPares(numero-1,count);
+        	   
         	} else {
         		numerosPares(numero-1,count);
+        		
         	}
     		}
-   
-            return count;
         }
-
-    
-   
-    
+	
     public static boolean isPar(int n) {
     	if(n%2==0 || n==0) {
     		return true;
